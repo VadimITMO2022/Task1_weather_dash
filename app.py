dd84433973aa633949dfb1b0ccd13ff7713f5626
Vadim Varshavskiy <vadim_varshavskiy@yahoo.com>
2025-04-13 22:14:58 +0300

    first commit

---
+++ app.py
@@ -0,0 +1,15 @@
+from dash import Dash
+from layouts import create_layout
+from callbacks import register_callbacks
+import dash_bootstrap_components as dbc
+
+app = Dash(external_stylesheets=[dbc.themes.DARKLY])
+app.title = "Состояние воздуха в городе"
+
+app.layout = create_layout()
+
+register_callbacks(app)
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
